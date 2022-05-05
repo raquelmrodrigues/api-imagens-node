1d4bd137989fa40b812ba6e07bb53a3d7bdf68f8
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-05-05 12:52:49 -0300

    conecção com o mongo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ let mongoose = require("mongoose");
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/dbfotos", {useNewUrlParser: true, useUnifieldTopology: true});
+mongoose.connect("mongodb://localhost:27017/dbfotos", {useNewUrlParser: true, useUnifieldTopology: true})
+    .then(() => {
+        console.log("conectado com o banco");
+    }).catch((err) => {
+        console.log(err);
+    });
 
 app.get("/", (req, res) => {
     res.json({});
