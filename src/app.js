c64ca81844511ea51bbb85a18a5ee74c7474bc53
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-05-05 14:39:04 -0300

    cadastro de usuários

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ let express = require("express");
 let app = express();
 let mongoose = require("mongoose");
 
+let user = require("../models/User")
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
@@ -12,8 +14,21 @@ mongoose.connect("mongodb://localhost:27017/dbfotos", {useNewUrlParser: true, us
         console.log(err);
     });
 
+let User = mongoose.model("User", user);
+
 app.get("/", (req, res) => {
     res.json({});
+})
+
+app.post("/user", async (req, res) => {
+    try {
+        let newUser = new User({name: req.body.name, email: req.body.email, password: req.body.password});
+        await newUser.save();
+        res.json({email: req.body.email})
+    }catch(err){
+        res.sendStatus(500);
+    }
+    
 })
 
 module.exports = app;
