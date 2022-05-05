c339780f715b0f48926c9e17e2166432fd06e33a
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-05-05 19:25:36 -0300

    teste e-mail duplicado

--- test/user.test.js
+++ test/user.test.js
@@ -4,6 +4,7 @@ const { getMaxListeners } = require("../src/app");
 let request = supertest(app);
 
 describe("Cadastro de usuário", () => {
+
     test("Deve cadastrar um usuário com sucesso", () => {
         
         //gera um email único para teste
@@ -34,4 +35,30 @@ describe("Cadastro de usuário", () => {
             fail(err);
         });
     }) 
+
+    test("Deve impedir que um usuário se cadastre com um e-mail repetido", () => {
+
+        let time = Date.now();
+        let email = `${time}@email.com}`;
+
+        let user = {name: "victor", email: email, password: "1234"};
+
+        return request.post("/user").send(user).then(res => {
+            
+            expect(res.statusCode).toEqual(200);
+            expect(res.body.email).toEqual(email);
+
+            return request.post("/user").send(user).then(res => {
+
+                expect(re.statusCode).toEqual(400);
+                expect(res.body.error).toEqual("E-mail já cadastrado");
+
+            }).catch(err => {
+                fail(err);
+            })
+
+        }).catch(err => {
+            fail(err);
+        });
+    })
 }) 
